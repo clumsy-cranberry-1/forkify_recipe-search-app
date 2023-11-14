c99915ad8f5e193f2da60749e191602951f1792b
Clumsy_Cranberry_1 <mlsshrbst@gmail.com>
2023-11-14 18:30:48 +0200

    clean up styles and layout

--- src/js/views/favouritesView.js
+++ src/js/views/favouritesView.js
@@ -13,6 +13,7 @@ class FavouritesView extends View {
     this._data = data;
     this._clearHtml();
     const html = this._generateHtml();
+    console.log(html);
     this._parentElement.insertAdjacentHTML('afterbegin', html);
   }
 
@@ -26,20 +27,20 @@ class FavouritesView extends View {
        ${this._data.map(recipe => {
       return `
           <li class="preview" id="${recipe.id}">
-            <a class="preview__link" href="#${recipe.id}">
-              <figure class="preview__fig">
+            <a class="favourites__link" href="#${recipe.id}">
+              <figure class="favourites__fig">
                 <img src="${recipe.imageUrl}" alt="Test" />
               </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">${recipe.title}</h4>
-                <p class="preview__publisher">${recipe.publisher}</p>
+              <div class="favourites__data">
+                <h4 class="favourites__title">${recipe.title}</h4>
+                <p class="favourites__publisher">${recipe.publisher}</p>
               </div>
             </a>
           </li>`;
-    })}
+    }).join("")}
     `;
-    // NOTE: "id" added to li item so that I can make use of the CSS ":target" pseudo class  
   }
 }
+// NOTE: "id" added to li item so that I can make use of the CSS ":target" pseudo class  
 
 export default new FavouritesView();
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -3,7 +3,7 @@ import View from './View.js';
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
   _data;
-  _errorMessage = `We could not find a recipe that matches your search.`
+  _errorMessage = `We could not find a recipe that matches your search.`;
 
   // public methods
   renderSearchResults(data) {
@@ -15,31 +15,29 @@ class ResultsView extends View {
     this._clearHtml();
     this._parentElement.insertAdjacentHTML('afterbegin', html);
   }
-
+  
+  // NOTE: "id" added to li item so that I can make use of the CSS ":target" pseudo class  
   _generateHtml() {
     return `
        ${this._data.map(recipe => {
-         return `
-          <li class="preview" id="${recipe.id}">
-            <a class="preview__link" href="#${recipe.id}">
-              <figure class="preview__fig">
-                <img src="${recipe.imageUrl}" alt="Test" />
-              </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">${recipe.title}</h4>
-                <p class="preview__publisher">${recipe.publisher}</p>
-                 <div class="preview__user-generated ${!recipe.key ? "hidden" : ""}">
-                  <svg>
-                    <use href="img/icons.svg#icon-user"></use>
-                  </svg>
-                </div>
-              </div>
-            </a>
-          </li>`;
-       })}
+      return `
+      <li class="preview" id="${recipe.id}">
+        <a class="preview__link" href="#${recipe.id}">
+          <figure class="preview__fig">
+            <img src="${recipe.imageUrl}" alt="img" />
+          </figure>
+          <div class="preview__data">
+            <h4 class="preview__title">${recipe.title}</h4>
+            <p class="preview__publisher">${recipe.publisher}</p>
+          </div>
+          <div class="preview__user-generated ${!recipe.key ? "hidden" : ""}">
+            <i class="fa-solid fa-user"></i>
+          </div>
+        </a>
+      </li>`;
+    }).join("")}
     `;
-    // NOTE: "id" added to li item so that I can make use of the CSS ":target" pseudo class  
   }
 }
-
+          
 export default new ResultsView();
